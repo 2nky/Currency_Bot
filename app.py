f61a3e83bf05ae07fba56062954763a1f75e1cb6
2nky <121765538+2nky@users.noreply.github.com>
2023-02-09 20:30:28 +0100

    распределение кода по файлам

--- app.py
+++ app.py
@@ -1,58 +1,11 @@
-import json
-import requests
 import telebot
 
-TOKEN = "6027550278:AAEDIJr4skazdPv0yinQ6rMws0SVJl4psUc"
-API_TOKEN = 'ALTXe7rVH2CZrBfWaJPcZ4FeBuUK9X3r4Jo1YWsl'
+from config import TOKEN
+from constants import keys
+from extensions import ConvertionException, GetPrice, EqualValues, QuoteException, BaseCurrencyException, AmountExeption
+
 
 bot = telebot.TeleBot(TOKEN)
-
-keys = {
-    "евро": "EUR",
-    "доллар": "USD",
-    "йена": "JPY"
-}
-class GetPrice:
-    @staticmethod
-    def get_price(quote: str, base: str, amount: float):
-
-        if quote == base:
-            raise EqualValues
-
-        if quote not in keys:
-            raise QuoteException
-
-        if base not in keys:
-            raise BaseException
-
-        quote_ticker, base_ticker = keys[quote], keys[base]
-        r = requests.get(
-            f'http://api.freecurrencyapi.com/v1/latest?apikey={API_TOKEN}&currencies={base_ticker}&base_currency={quote_ticker}'
-        )
-        total_base = json.loads(r.content)["data"][keys[base]]
-        return total_base * amount
-
-
-
-class ConvertionException(Exception):
-    def __str__(self):
-        return "Неверное количествоа параметров.\nВведи команду еще раз."
-
-class QuoteException(Exception):
-    def __str__(self):
-        return "Введена несущетсвующая переводимая валюта.\nВведи команду еще раз."
-
-class BaseException(Exception):
-    def __str__(self):
-        return "Введена несущетсвующая валюта для перевода.\n Введи команду еще раз."
-
-class EqualValues(Exception):
-    def __str__(self):
-        return "Одинаковые валюты для конвртации.\n Введи команду еще раз."
-
-class AmountExeption(Exception):
-    def __str__(self):
-        return "Количесто валюты должно быть числом.\n Введи команду еще раз."
 
 
 @bot.message_handler(commands=["start", "help"])
@@ -88,7 +41,7 @@ def convert(messege: telebot.types.Message):
     except QuoteException:
         bot.reply_to(messege, "Введена несущетсвующая переводимая валюта.\nВведи команду еще раз.")
 
-    except BaseException:
+    except BaseCurrencyException:
         bot.reply_to(messege, "Введена несущетсвующая валюта для перевода.\n Введи команду еще раз.")
 
     except ConvertionException:
---
+++ extensions.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+from config import API_TOKEN
+from constants import keys
+
+
+class GetPrice:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: float):
+
+        if quote == base:
+            raise EqualValues
+
+        if quote not in keys:
+            raise QuoteException
+
+        if base not in keys:
+            raise BaseCurrencyException
+
+        quote_ticker, base_ticker = keys[quote], keys[base]
+        r = requests.get(
+            f'http://api.freecurrencyapi.com/v1/latest?apikey={API_TOKEN}&currencies={base_ticker}&base_currency={quote_ticker}'
+        )
+        total_base = json.loads(r.content)["data"][keys[base]]
+        return total_base * amount
+
+
+
+class ConvertionException(Exception):
+    def __str__(self):
+        return "Неверное количествоа параметров.\nВведи команду еще раз."
+
+class QuoteException(Exception):
+    def __str__(self):
+        return "Введена несущетсвующая переводимая валюта.\nВведи команду еще раз."
+
+class BaseCurrencyException(Exception):
+    def __str__(self):
+        return "Введена несущетсвующая валюта для перевода.\n Введи команду еще раз."
+
+class EqualValues(Exception):
+    def __str__(self):
+        return "Одинаковые валюты для конвртации.\n Введи команду еще раз."
+
+class AmountExeption(Exception):
+    def __str__(self):
+        return "Количесто валюты должно быть числом.\n Введи команду еще раз."
